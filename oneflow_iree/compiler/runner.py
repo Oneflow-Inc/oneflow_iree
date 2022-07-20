30e7dec07ca11287d1317296e08b12d815a013fe
yuhao <1171760467@qq.com>
2022-07-20 13:16:04 +0800

    convert job to tosa api amend

--- oneflow_iree/compiler/runner.py
+++ oneflow_iree/compiler/runner.py
@@ -62,7 +62,8 @@ class Iree(Backend):
         self.vm_module = ireert.VmModule.from_flatbuffer(self.flat_buffer)
 
     def _get_job(self):
-        self.job = str(text_format.MessageToString(self.graph._full_job_proto))
+        self.job = self.graph._full_job_proto.SerializeToString()
+
 
     def _convert_job_to_tosa(self):
         self.tosa = flow._oneflow_internal.nn.graph.ConvertJobToTosaIR(self.job)
