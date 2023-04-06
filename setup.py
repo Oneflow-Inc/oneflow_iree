c17732b50abf1b070a4b2dd638372ba585ef2ddd
yuhao <1171760467@qq.com>
2023-04-06 07:54:22 +0000

    new

--- setup.py
+++ setup.py
@@ -16,8 +16,8 @@ import setuptools
 
 reqs = [
         'protobuf',
-        'iree-compiler==20230228.444',
-        'iree-runtime==20230228.444'
+        'iree-compiler==20230209.425',
+        'iree-runtime==20230209.425'
     ]
 
 long_description = (
